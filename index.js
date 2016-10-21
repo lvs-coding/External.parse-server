58f4ee5332b285048780dcc6f4bfa2a276f51d79
Laurent <lvs.freelance@gmail.com>
2016-10-21 11:24:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'Hx2hHaGGQL6erebrWxVGfpv6fWaA494nF6KxKDPG', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://localhost:1337/parse',  // Don't forget to change to https if needed
    javascriptKey: process.env.JAVASCRIPT_KEY || '',  //** add this line no need to set values, they will be overwritten by heroku config vars
   liveQuery: {
